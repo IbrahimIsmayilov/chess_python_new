0d78deb213cb7580ff04895f15424d058ec4ee27
Ibrahim Ismayilov <ibrahiimismayil@gmail.com>
2026-08-17 23:25:29 -0600

    setting dimensions

--- ChessMain.py
+++ ChessMain.py
@@ -1,3 +1,19 @@
 """
 This is our main driver file. It will be responsible for handling user input and displaying the current GameState object. 
 """
+
+import pygame as p
+import ChessEngine
+
+WIDTH = HEIGHT = 512 # 400 is another option
+DIMENSION = 8 # dimensions of a chess board are 8x8
+SQ_SIZE = HEIGHT // DIMENSION
+MAX_FPS = 15 # for animations later on
+IMAGES = {}
+
+'''
+Initialize a global dictionary of images. This will be called exactly once in the main
+'''
+
+def loadImages():
+    IMAGES['wP'] = p.image.load("images/wP.png")
